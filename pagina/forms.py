fa7f3e424c3ebef34246267a3709938cef25780e
Jonathan <trowsing@gmail.com>
2019-03-05 23:23:20 -0400

    Adding styles.

--- pagina/forms.py
+++ pagina/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from .models import Heroes
 
 class HeroesForm(forms.ModelForm):
+
     class Meta:
         model = Heroes
-        fields = ('name', 'description', 'image', )
+        fields = ['name', 'description', 'image']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'nes-input', 'style':'width: 500px;'}),
+            'description': forms.Textarea(attrs={'class': 'nes-textarea', 'style': 'width: 600px;'})
+        }
